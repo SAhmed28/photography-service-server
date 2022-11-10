0d7dc65d70c81a5348ab60a76f2c2d2f10659971
SAhmed28 <sanjida1@ualberta.ca>
2022-11-09 23:30:12 -0600

    DB initial setup done

--- index.js
+++ index.js
@@ -10,6 +10,19 @@ app.use(cors());
 app.use(express.json());
 
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.oplybtq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const serviceCollection = client.db('photography').collection('services');
+    }
+    finally{
+
+    }
+}
+run().catch(err => console.error(err))
+
 app.get('/', (req,res) => {
     res.send('Photography service server is running');
 })
